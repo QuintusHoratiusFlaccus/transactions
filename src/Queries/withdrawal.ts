75f7d7cd7de84b33d4b64c9834c3e72d21828764
QuintusHoratiusFlaccus <konnor1122@gmail.com>
2021-07-20 13:26:42 +0300

    modify withdrawalQuery, added filter

--- src/Queries/withdrawal.ts
+++ src/Queries/withdrawal.ts
@@ -2,11 +2,7 @@ import {gql} from "@apollo/client";
 
 export const GET_WITHDRAWAL = gql`
     query GetWithdrawals(
-        $status: [String!]
-        $id: ID,
-        $playerId: ID,
-        $currency: [String!],
-        $isLocked: Boolean
+        $filter: WithdrawalFilters
     ) {
         Withdrawals(
             filter: $filter
